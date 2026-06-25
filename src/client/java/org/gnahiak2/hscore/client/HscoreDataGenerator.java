acf73b38958b3e5e761f4cf138cbf705c2d9bd3d
gnahiak2 <wangz9096z@gmail.com>
2026-06-25 15:05:14 +0800

    Initial commit on development branch

---
+++ src/client/java/org/gnahiak2/hscore/client/HscoreDataGenerator.java
@@ -0,0 +1,12 @@
+package org.gnahiak2.hscore.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HscoreDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/org/gnahiak2/hscore/Hscore.java
@@ -0,0 +1,10 @@
+package org.gnahiak2.hscore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Hscore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
