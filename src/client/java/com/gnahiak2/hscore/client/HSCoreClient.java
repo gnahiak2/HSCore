670627ceb809bcb91c9dca34ed2b975bd0a54507
gnahiak2 <wangz9096z@gmail.com>
2026-06-27 15:37:54 +0800

    Replace old jetbrains project with new one

---
+++ src/client/java/com/gnahiak2/hscore/client/HSCoreClient.java
@@ -0,0 +1,10 @@
+package com.gnahiak2.hscore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HSCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
